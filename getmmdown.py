fb0a83431f68e351d54a30dafd225129ec9fd5be
slayerten <slayerten@163.com>
2016-10-23 20:57:19 +0800

    get meitulu.com pics

---
+++ getmmdown.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import urllib
+import re
+
+
+num = 7926
+html = 'http://www.meitulu.com/item/%d.html'% num
+n = 770
+page = urllib.urlopen(html)
+html_yuan = page.read()
+reg = r'src=(http.+?\d{17}\.jpg)'
+imgreg = re.compile(reg)
+imglist = re.findall(imgreg,html_yuan)
+print imglist
+for img_false in imglist:
+    urllib.urlretrieve(img_false,'girl%d.jpg'% n)
+    n += 1
+print n
+m = n
+for x in range(2,14):
+    html ='http://www.meitulu.com/item/%d_%s.html'%(num,x)
+    print html
+    page = urllib.urlopen(html)
+    html_yuan = page.read()
+    reg = r'src=(http.+?\d{17}\.jpg)'
+    imgreg = re.compile(reg)
+    imglist = re.findall(imgreg,html_yuan)
+    print imglist
+    for img_false in imglist:
+        urllib.urlretrieve(img_false,'girl%d.jpg'% m)
+        m += 1
+print m
+

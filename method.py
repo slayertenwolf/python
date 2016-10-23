5fb40943f5b2a0f4054027c33be711d8665f907f
slayerten <slayerten@163.com>
2016-10-23 20:45:58 +0800

    change  method.py

--- method.py
+++ method.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python
 
+"""
 class Persion:
     def sayHi(self):
         print 'Hello,how are you?My lady?'
 
 p = Persion()
 p.sayHi()
+"""
+
+class Hero:
+    def SayHi(self):
+        print "Hello,how are you?"
+
+
+Wolf = Hero()
+Wolf.SayHi()
